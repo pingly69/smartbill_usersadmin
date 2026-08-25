01a193b58921e90bc406fd76798e753b535433c4
pingly69 <pingly69@gmail.com>
2026-08-25 12:57:36 +0700

    [v1.0.1] Update production GAS Web App URL and LIFF ID

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Configuration variables for SmartBill Users Admin
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzResnXlQ3FNOtlXepfjLMtPIKETxzqedKEqQ3nOvZ06AvQYDYn-W5yZ5b7sJ4dhAld/exec";
-const LIFF_ID = "2009016720-Wih8NJa6";
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxRqnq-abyv8SCAWJywdeaAiyUrNc37P191sdFIiaRQIDewyXpY5PDjA2bHaJhgts_4/exec";
+const LIFF_ID = "2009016720-7Bu2JHRK";
 const APP_VERSION = "v1.0.0";
