2534a73e35b12362f31751e1b0f230751d32f4c8
pingly69 <pingly69@gmail.com>
2026-08-25 14:06:19 +0700

    [config] Use correct LIFF_ID 2009016720-7Bu2JHRK

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Configuration variables for SmartBill Users Admin
 const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzResnXlQ3FNOtlXepfjLMtPIKETxzqedKEqQ3nOvZ06AvQYDYn-W5yZ5b7sJ4dhAld/exec";
-const LIFF_ID = "2009016720-Wih8NJa6";
+const LIFF_ID = "2009016720-7Bu2JHRK";
 const APP_VERSION = "v1.0.0";
